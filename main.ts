98938f28309b9e16a122131768bb1498ba7a4a00
Vin ☆ <vinnie.may@student.onslow.school.nz>
2026-09-11 15:07:18 +1200

    intro section

--- main.ts
+++ main.ts
@@ -6,11 +6,29 @@ namespace SpriteKind {
     export const PowerUpTut = SpriteKind.create()
     export const Trapdoor = SpriteKind.create()
     export const questionTUT = SpriteKind.create()
+    export const tutbutton1 = SpriteKind.create()
+    export const press = SpriteKind.create()
+    export const tutbutton2 = SpriteKind.create()
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Temp_Player.vy == 0) {
         Temp_Player.vy = -200
     }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.tutbutton1, function (sprite, otherSprite) {
+    sprites.destroy(button)
+    sprites.destroy(press)
+    sprites.destroy(Question_1_tut)
+    sprites.destroy(Question1Tut2)
+    sprites.destroy(Question1Tut3)
+    sprites.destroy(Question1Tut4)
+    questtut5 = sprites.create(assets.image`text1`, SpriteKind.questionTUT)
+    questtut5.setPosition(122, 460)
+    pause(3000)
+    button = sprites.create(assets.image`button`, SpriteKind.tutbutton2)
+    button.setPosition(174, 505)
+    press = sprites.create(assets.image`arrow`, SpriteKind.press)
+    press.setPosition(175, 482)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     Temp_Player.setImage(assets.image`Vamp Main LEFT proper`)
@@ -40,6 +58,17 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Tut2, function (sprite, otherSpr
     game.splash("Press W to jump")
     sprites.destroy(Tut2sprite)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.tutbutton2, function (sprite, otherSprite) {
+    sprites.destroy(button)
+    sprites.destroy(press)
+})
+let questtut5: Sprite = null
+let press: Sprite = null
+let button: Sprite = null
+let Question1Tut4: Sprite = null
+let Question1Tut3: Sprite = null
+let Question1Tut2: Sprite = null
+let Question_1_tut: Sprite = null
 let Speed_Boost: Sprite = null
 let Tut3sprite: Sprite = null
 let Tut2sprite: Sprite = null
@@ -49,14 +78,6 @@ scene.setBackgroundColor(13)
 Temp_Player = sprites.create(assets.image`Vamp Main RIGHT`, SpriteKind.Player)
 Temp_Player.setPosition(17, 120)
 Temp_Player.setPosition(119, 483)
-let Question_1_tut = sprites.create(assets.image`welcome`, SpriteKind.questionTUT)
-Question_1_tut.setPosition(118, 443)
-let Question1Tut2 = sprites.create(assets.image`the`, SpriteKind.questionTUT)
-Question1Tut2.setPosition(88, 461)
-let Question1Tut3 = sprites.create(assets.image`vampires 4`, SpriteKind.questionTUT)
-Question1Tut3.setPosition(132, 457)
-let Question1Tut4 = sprites.create(assets.image`castle 3`, SpriteKind.questionTUT)
-Question1Tut4.setPosition(123, 474)
 scene.cameraFollowSprite(Temp_Player)
 controller.moveSprite(Temp_Player, 120, 0)
 tiles.setCurrentTilemap(tilemap`VAMPIRE MAP`)
@@ -71,3 +92,15 @@ Speed_Boost = sprites.create(assets.image`Blood Bottle`, SpriteKind.PowerUpTut)
 Speed_Boost.setPosition(392, 57)
 let Trapdoor2 = sprites.create(assets.image`Trapdoor`, SpriteKind.Trapdoor)
 Trapdoor2.setPosition(624, 136)
+Question_1_tut = sprites.create(assets.image`welcome`, SpriteKind.questionTUT)
+Question_1_tut.setPosition(118, 439)
+Question1Tut2 = sprites.create(assets.image`the`, SpriteKind.questionTUT)
+Question1Tut2.setPosition(88, 457)
+Question1Tut3 = sprites.create(assets.image`vampires 4`, SpriteKind.questionTUT)
+Question1Tut3.setPosition(132, 453)
+Question1Tut4 = sprites.create(assets.image`castle something`, SpriteKind.questionTUT)
+Question1Tut4.setPosition(119, 471)
+button = sprites.create(assets.image`button`, SpriteKind.tutbutton1)
+button.setPosition(174, 505)
+press = sprites.create(assets.image`press to continue`, SpriteKind.press)
+press.setPosition(178, 482)
